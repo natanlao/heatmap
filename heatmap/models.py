0498f906ff765dbc08d28e90c78b206ea8ba4e50
Natan Lao <natanlao@users.noreply.github.com>
2018-01-22 06:04:22 +0000

    Work

--- heatmap/models.py
+++ heatmap/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from peewee import *
 
-#db = SqliteDatabase('heatmap.db')
-db = SqliteDatabase(":memory:")
+db = SqliteDatabase('heatmap.db')
+#db = SqliteDatabase(":memory:")
 
 class Subnet(Model):
-    comment = CharField()
+    comment = CharField(null=True)
     network = CharField()
     subnet  = CharField()
     gateway = CharField()
@@ -18,7 +18,6 @@ class Listing(Model):
     fixed = IntegerField()
     pool = IntegerField()
     assigned = IntegerField()
-    #asof = DateTimeField(formats=["%a %b %d %H:%M:%S PST %Y"])
     asof = DateTimeField()
 
     class Meta:
--- run.py
+++ run.py
@@ -4,10 +4,11 @@ import email.utils
 import time
 import os.path
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
+import pytz
 import requests
 
-from heatmap.model import db, Listing, Subnet
+from heatmap.models import db, Listing, Subnet
 
 if not os.path.exists("heatmap.db"):
     db.create_tables([Subnet, Listing])
@@ -19,11 +20,12 @@ def get_data():
 
 
 def parse_data(html):
-    s = BeautifulSoup(html, "html.parser")
+    s = BeautifulSoup(html, "lxml", parse_only=SoupStrainer(["b", "table"]))
 
     newtime = email.utils.parsedate_to_datetime(s.b.text.strip())
     try:
         oldtime = Listing.select().order_by(Listing.asof.desc()).get().asof
+        oldtime = datetime.datetime.strptime(oldtime, "%Y-%m-%d %H:%M:%S-08:00").replace(tzinfo=pytz.timezone("US/Pacific"))
         if not (newtime > oldtime):
             return
     except Listing.DoesNotExist:
@@ -32,25 +34,36 @@ def parse_data(html):
 
     # Skip header row and isolate table
     rows = s.find_all("tr")
+    listings = []
     for row in rows[1:]:
         data = dict(zip([i.text.strip() for i in rows[0].findChildren("th")], \
                         [i.text.strip() for i in row.children]))
-        snet = Subnet.get_or_create(
+        snet, _ = Subnet.get_or_create(
                 comment=data['Comment'],
                 network=data['Network'],
                 subnet=data['Subnet'],
                 gateway=data['Gateway']
         )
-        Listing.create(
-                subnet=snet,
-                fixed=int(data['Fixed']),
-                pool=int(data['Pool']),
-                assigned=int(data['Assigned']), asof="2018-01-01 01:01:01"
-                #asof=str(newtime.strftime("%Y-%m-%d %H:%M:%S"))
-        )
+        listings.append({
+            'subnet': snet.id,
+            'fixed': data['Fixed'],
+            'pool': data['Pool'],
+            'assigned': data['Assigned'],
+            'asof': newtime
+        })
+
+    with db.atomic():
+        print("Inserting %d records" % len(listings))
+        Listing.insert_many(listings).execute()
 
 if __name__ == "__main__":
     get_data()
+    exit()
+    import os.path
+    for f in sorted(os.listdir("data"), key=lambda x: os.path.getmtime("data/"+x)):
+        print(f)
+        with open(os.path.join("data", f), "r") as html:
+            parse_data(html)
     exit()
     while True:
         get_data()
